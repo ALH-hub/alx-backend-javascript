542373cc1097a14144d7d41013adb9d787f4022f
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-23 09:17:06 +0100

    var and hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    let Task = true;
-    let Task2 = false;
+    var Task = true;
+    var Task2 = false;
   }
 
   return [task, task2];
