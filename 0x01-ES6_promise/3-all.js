746f5d3fc0ce27427cc2f5b44b1f3fa486001160
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-24 10:47:13 +0100

    resolve all promises using all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,18 +1,20 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([
-    uploadPhoto().catch((error) => {
-      console.error('Error uploading photo:', error);
-      throw error;
-    }),
-    createUser().catch((error) => {
-      console.error('Error creating user:', error);
-      throw error;
-    }),
-  ])
-    .then((values) => {
-      console.log(values[0].body, values[1].firstName, values[1].lastName);
+  let body;
+  let firstName;
+  let lastName;
+
+  return uploadPhoto()
+    .then((data) => {
+      body = data.body;
+      createUser()
+        .then((data) => {
+          firstName = data.firstName;
+          lastName = data.lastName;
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
     .catch(() => console.log('Signup system offline'));
 }
