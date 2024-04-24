f8ca3619b1404b7d9965fbbc187c5e4c1e0c3f0f
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-25 00:17:12 +0100

    try catch function

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    console.error(error);
+  }
+  return { photo, user };
+}
