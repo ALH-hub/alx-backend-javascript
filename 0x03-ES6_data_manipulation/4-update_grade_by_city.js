767f04d0a26a9979bfd4f6fe3b4f0aee99b30728
Alhadji Oumate <alhadjioum0@gmail.com>
2024-04-30 22:47:24 +0100

    array combinint filter and map methods

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 export default function updateStudentGradeByCity(array, city, newGrades) {
-  if (Array.isArray(array) || Array.isArray(newGrades) || typeof city !== 'string') {
+  if (Array.isArray(array) && Array.isArray(newGrades) && typeof city === 'string') {
     return array.filter((student) => student.location === city).map((student) => {
       const [newGrade] = newGrades.filter((item) => item.studentId === student.id);
       return { ...student, grade: newGrade ? newGrade.grade : 'N/A' };
