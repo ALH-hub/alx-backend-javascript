27f6ec01fc7ba33ff44d732f8d240a2e54518a94
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-18 15:36:24 +0100

    stdin requirement

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,18 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
+process.stdin.resume();
+
 process.stdin.on('data', (data) => {
+  const val = process.stdin.read();
   process.stdout.write(`Your name is: ${data}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
+   process.stdout.write('This important software is now closing\n');
+   process.exit();
 });
+
+//process.stdin.on('data', (data) => {
+//  process.stdout.write(`Your name is: ${data}`);
+//});
