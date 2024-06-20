fee019b7906e286e051ef5b0168f00e4413ad120
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-20 19:50:19 +0100

    test with chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,10 +1,12 @@
 const calculateNumber = require('./2-calcul_chai.js');
-const chai = require('chai');
-const expect = chai.expect;
+const { expect } = require('chai');
 
 describe('calculateNumber', () => {
-	it('checks the output of the function', () => {
-		expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+	it('round and add', () => {
+		expect(calculateNumber('SUM', 1, 3) === 4);
+	});
+
+	it('round and sum', () => {
 		expect(calculateNumber('SUM', 1.9, 3)).to.equal(5);
 		expect(calculateNumber('SUM', 1.9, 3.1)).to.equal(5);
 		expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
@@ -25,6 +27,8 @@ describe('calculateNumber', () => {
 		expect(calculateNumber('SUM', 0.1, 0.9999999)).to.equal(1);
 		expect(calculateNumber('SUM', 0.1, 0.99999999)).to.equal(1);
 		expect(calculateNumber('SUM', 0.1, 0.999999999)).to.equal(1);
+	});
+	it('round and subtract', () => {
 		expect(calculateNumber('SUBRACT', 1, 3)).to.equal(-2);
 		expect(calculateNumber('SUBRACT', 1.9, 3)).to.equal(-1);
 		expect(calculateNumber('SUBRACT', 1.9, 3.1)).to.equal(-1);
@@ -36,6 +40,8 @@ describe('calculateNumber', () => {
 		expect(calculateNumber('SUBRACT', 0.1, 0.5)).to.equal(0);
 		expect(calculateNumber('SUBRACT', 0.1, 0.4)).to.equal(0);
 		expect(calculateNumber('SUBRACT', 0.1, 0.49)).to.equal(0);
+	});
+	it('round and divide', () => {
 		expect(calculateNumber('DIVIDE', 1, 3)).to.equal(0.3333333333333333);
 		expect(calculateNumber('DIVIDE', 1.9, 3)).to.equal(0.6333333333333333);
 		expect(calculateNumber('DIVIDE', 1.9, 3.1)).to.equal(0.6129032258064516);
