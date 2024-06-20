62312dbcfc49f5e35c6143b12b9c56439ca619ac
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-20 19:38:17 +0100

    test with chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,6 @@
 const calculateNumber = require('./2-calcul_chai.js');
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
 
 describe('calculateNumber', () => {
 	it('checks the output of the function', () => {
