d425bf685f484c97285bb2ecd95c4345a726bffb
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-21 22:11:21 +0100

    test api routes

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,39 +6,37 @@ const app = require('./api.js');
 
 chai.use(chaiHttp);
 
-describe('API Tests', () => {
-	describe('GET /', () => {
-		it('should return status code 200', (done) => {
-			chai
-				.request(app)
-				.get('/')
-				.end((err, res) => {
-					expect(err).to.be.null;
-					expect(res).to.have.status(200);
-					done();
-				});
-		});
+describe('Index page', () => {
+	it('should return status code 200', (done) => {
+		chai
+			.request(app)
+			.get('/')
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
+				done();
+			});
+	});
 
-		it('should return the message "Welcome to the payment system"', (done) => {
-			chai
-				.request(app)
-				.get('/')
-				.end((err, res) => {
-					expect(err).to.be.null;
-					expect(res.text).to.equal('Welcome to the payment system');
-					done();
-				});
-		});
+	it('should return the message "Welcome to the payment system"', (done) => {
+		chai
+			.request(app)
+			.get('/')
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res.text).to.equal('Welcome to the payment system');
+				done();
+			});
+	});
 
-		it('test port number 7865', (done) => {
-			chai
-				.request(app)
-				.get('/')
-				.end((err, res) => {
-					expect(err).to.be.null;
-					expect(res).to.have.status(200);
-					done();
-				});
-		});
+	it('test port number 7865', (done) => {
+		chai
+			.request(app)
+			.get('/')
+			.end((err, res) => {
+				expect(err).to.be.null;
+				expect(res).to.have.status(200);
+				done();
+			});
 	});
 });
